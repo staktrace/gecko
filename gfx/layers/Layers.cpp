c3971d6986d8684a3f2d5ae37a0a6e046fcf0206
Kartikaya Gupta <kgupta@mozilla.com>
2019-03-05 12:17:38 -0500

    Guard against BasicLayerManager putting pending scroll updates in the content RR bucket and not being able to get them back out

--- gfx/layers/Layers.cpp
+++ gfx/layers/Layers.cpp
@@ -2236,12 +2236,21 @@ bool LayerManager::SetPendingScrollUpdateForNextTransaction(
     return false;
   }
 
-  mPendingScrollUpdates[aRenderRoot][aScrollId] = aUpdateInfo;
+  // If this is called on a LayerManager that's not a WebRenderLayerManager,
+  // then we don't actually need the aRenderRoot information. We force it to
+  // RenderRoot::Default so that we can make assumptions in
+  // GetPendingScrollInfoUpdate.
+  wr::RenderRoot renderRoot = (GetBackendType() == LayersBackend::LAYERS_WR)
+    ? aRenderRoot : wr::RenderRoot::Default;
+  mPendingScrollUpdates[renderRoot][aScrollId] = aUpdateInfo;
   return true;
 }
 
 Maybe<ScrollUpdateInfo> LayerManager::GetPendingScrollInfoUpdate(
     ScrollableLayerGuid::ViewID aScrollId) {
+  // This never gets called for WebRenderLayerManager, so we assume that all
+  // pending scroll info updates are stored under the default RenderRoot.
+  MOZ_ASSERT(GetBackendType() != LayersBackend::LAYERS_WR);
   auto it = mPendingScrollUpdates[wr::RenderRoot::Default].find(aScrollId);
   if (it != mPendingScrollUpdates[wr::RenderRoot::Default].end()) {
     return Some(it->second);
