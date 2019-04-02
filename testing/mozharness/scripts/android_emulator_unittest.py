9dad60b8dedc85e1cda8152218e7b8b6625abf31
Kartikaya Gupta <kgupta@mozilla.com>
2019-04-02 13:37:24 -0400

    WR for android unittests

--- testing/mozharness/scripts/android_emulator_unittest.py
+++ testing/mozharness/scripts/android_emulator_unittest.py
@@ -87,6 +87,20 @@ class AndroidEmulatorTest(TestingMixin, BaseScript, MozbaseMixin, CodeCoverageMi
          "default": False,
          "help": "Run tests with multiple processes.",
          }
+    ], [
+        ['--enable-webrender'],
+        {"action": "store_true",
+         "dest": "enable_webrender",
+         "default": False,
+         "help": "Run with WebRender enabled.",
+         }
+    ], [
+        ['--disable-webrender'],
+        {"action": "store_true",
+         "dest": "disable_webrender",
+         "default": False,
+         "help": "Run with WebRender force-disabled.",
+         }
     ]] + copy.deepcopy(testing_config_options) + \
         copy.deepcopy(code_coverage_config_options)
 
@@ -134,6 +148,8 @@ class AndroidEmulatorTest(TestingMixin, BaseScript, MozbaseMixin, CodeCoverageMi
         self.log_raw_level = c.get('log_raw_level')
         self.log_tbpl_level = c.get('log_tbpl_level')
         self.e10s = c.get('e10s')
+        self.enable_webrender = c.get('enable_webrender')
+        self.disable_webrender = c.get('disable_webrender')
 
     def query_abs_dirs(self):
         if self.abs_dirs:
@@ -265,6 +281,11 @@ class AndroidEmulatorTest(TestingMixin, BaseScript, MozbaseMixin, CodeCoverageMi
                     cmd.extend(['--this-chunk', self.this_chunk])
                 if self.total_chunks is not None:
                     cmd.extend(['--total-chunks', self.total_chunks])
+
+        if self.disable_webrender:
+            cmd.extend(['--setenv', 'MOZ_WEBRENDER=0'])
+        elif self.enable_webrender:
+            cmd.extend(['--setenv', 'MOZ_WEBRENDER=1'])
 
         try_options, try_tests = self.try_args(self.test_suite)
         cmd.extend(try_options)
